4f0910ff274dc0aeedd0d9d319f81b9581422aea
Mikhail Melnik <by.zumzoom@gmail.com>
2021-03-03 15:39:10 +0300

    linter

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,28 +2,27 @@ require('@nomiclabs/hardhat-truffle5');
 require('solidity-coverage');
 require('hardhat-gas-reporter');
 
-
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-  solidity: {
-    version: "0.6.12",
-    settings: {
-      optimizer: {
-        enabled: true,
-        runs: 1000000,
-      },
+    solidity: {
+        version: '0.6.12',
+        settings: {
+            optimizer: {
+                enabled: true,
+                runs: 1000000,
+            },
+        },
     },
-  },
-  networks: {
-    hardhat: {
-      blockGasLimit: 10000000,
+    networks: {
+        hardhat: {
+            blockGasLimit: 10000000,
+        },
     },
-  },
-  gasReporter: {
-    enable: true,
-    currency: 'USD',
-    outputFile: process.env.CI ? 'gas-report.txt' : undefined,
-  },
+    gasReporter: {
+        enable: true,
+        currency: 'USD',
+        outputFile: process.env.CI ? 'gas-report.txt' : undefined,
+    },
 };
