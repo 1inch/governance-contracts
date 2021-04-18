60309d2becac23762976808d54d2aed301d45aa7
Mikhail Melnik <by.zumzoom@gmail.com>
2021-04-18 20:20:53 +0300

    minimal vesting

--- contracts/StepVesting.sol
+++ contracts/StepVesting.sol
@@ -5,23 +5,19 @@ pragma solidity ^0.6.0;
 import "@openzeppelin/contracts/math/Math.sol";
 import "@openzeppelin/contracts/math/SafeMath.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/utils/Pausable.sol";
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
 
 
-contract StepVesting is Ownable, Pausable {
+contract StepVesting is Ownable {
     using SafeMath for uint256;
     using SafeERC20 for IERC20;
 
     event ReceiverChanged(address oldWallet, address newWallet);
 
-    uint256 public immutable deployed;
+    uint256 public immutable startTimetamp;
     IERC20 public immutable token;
-    address public immutable source;
     uint256 public immutable cliffDuration;
     uint256 public immutable stepDuration;
-    uint256 public immutable cliffAmount;
     uint256 public immutable stepAmount;
     uint256 public immutable numOfSteps;
 
@@ -35,20 +31,17 @@ contract StepVesting is Ownable, Pausable {
 
     constructor(
         IERC20 _token,
-        address _source,
+        uint256 _startTimestamp,
         uint256 _cliffDuration,
         uint256 _stepDuration,
-        uint256 _cliffAmount,
         uint256 _stepAmount,
         uint256 _numOfSteps,
         address _receiver
     ) public {
-        deployed = block.timestamp;
+        startTimetamp = _startTimestamp;
         token = _token;
-        source = _source;
         cliffDuration = _cliffDuration;
         stepDuration = _stepDuration;
-        cliffAmount = _cliffAmount;
         stepAmount = _stepAmount;
         numOfSteps = _numOfSteps;
         setReceiver(_receiver);
@@ -59,15 +52,14 @@ contract StepVesting is Ownable, Pausable {
     }
 
     function claimable() public view returns(uint256) {
-        if (block.timestamp < deployed.add(cliffDuration)) {
+        uint256 firstClaim = startTimetamp.add(cliffDuration);
+        if (block.timestamp < firstClaim) {
             return 0;
         }
 
-        uint256 passedSinceCliff = block.timestamp.sub(deployed.add(cliffDuration));
-        uint256 stepsPassed = Math.min(numOfSteps, passedSinceCliff.div(stepDuration));
-        return cliffAmount.add(
-            stepsPassed.mul(stepAmount)
-        );
+        uint256 passedSinceCliff = block.timestamp.sub(firstClaim);
+        uint256 stepsPassed = Math.min(numOfSteps, passedSinceCliff.div(stepDuration).add(1));
+        return stepsPassed.mul(stepAmount);
     }
 
     function setReceiver(address _receiver) public onlyOwner {
@@ -75,13 +67,14 @@ contract StepVesting is Ownable, Pausable {
         receiver = _receiver;
     }
 
-    function kill() external onlyOwner {
-        _pause();
+    function kill(address target) external onlyOwner {
+        uint256 amount = token.balanceOf(address(this));
+        token.safeTransfer(target, amount);
     }
 
-    function claim() external onlyReceiver whenNotPaused {
+    function claim() external onlyReceiver {
         uint256 amount = available();
         claimed = claimed.add(amount);
-        token.safeTransferFrom(source, msg.sender, amount);
+        token.safeTransfer(msg.sender, amount);
     }
 }
