97add372a6e183d8ad2bc7debd28359d5cd2f1ff
Mikhail Melnik <by.zumzoom@gmail.com>
2021-04-19 13:44:18 +0300

    fix

--- contracts/StepVesting.sol
+++ contracts/StepVesting.sol
@@ -5,8 +5,6 @@ pragma solidity ^0.6.0;
 import "@openzeppelin/contracts/math/Math.sol";
 import "@openzeppelin/contracts/math/SafeMath.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/utils/Pausable.sol";
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
 
 
@@ -78,7 +76,7 @@ contract StepVesting is Ownable {
         token.safeTransfer(target, amount);
     }
 
-    function claim() external onlyReceiver whenNotPaused {
+    function claim() external onlyReceiver {
         uint256 amount = available();
         claimed = claimed.add(amount);
         token.safeTransfer(msg.sender, amount);
